1645d3684c53fae1e04dbd1845e88d6ea5e2e343
isamarsharma88 <isamarsharma88@gmail.com>
2025-11-15 23:38:18 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // firebase-config.js
 export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
+  apiKey: "AIzaSyCNF0tg_Jrt-8iL8Azx2rY8zCVdaJU2l9g",
   authDomain: "job-board-da315.firebaseapp.com",
   projectId: "job-board-da315",
-  storageBucket: "job-board-da315.appspot.com",
+  storageBucket: "job-board-da315.firebasestorage.app",
   messagingSenderId: "908048807079",
   appId: "1:908048807079:web:58f70af2e706f27684d782",
   measurementId: "G-FHYPY906DE"
